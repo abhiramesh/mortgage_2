4fd707c24cb8eb03fefaecbf10504c2617b3a5e8
Abhi Ramesh <abhiramesh@gmail.com>
2013-04-05 20:19:48 -0700

    new front page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,10 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :zipcode, :home_value, :mortgage_balance, :street, :state, :city, :name, :email, :phone
+
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  
+  #validates :zipcode, presence: true
  
 end
