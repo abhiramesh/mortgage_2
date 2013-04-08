a4910897bbf13c7ef3e47773e557c6359837cdcf
Abhi Ramesh <abhiramesh@gmail.com>
2013-04-08 01:03:07 -0700

    Back problem, custom JS validations, rake assets

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,18 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :zipcode, :home_value, :mortgage_balance, :street, :state, :city, :name, :email, :phone
+  
+  validates :zipcode, presence: true
+  validates_format_of :zipcode, :with => /^\d{5}(-\d{4})?$/, :message => "Please enter a correct zipcode"
+
+  #validates :home_value, presence: true, :numericality => { :only_integer => true }, :on => :update
+  #validates :mortgage_balance, presence: true, :numericality => { :only_integer => true }, :on => :update
 
   #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   
-  #validates :zipcode, presence: true
+  #validates :phone, presence: true, :numericality => { :only_integer => true },  :length => { :is => 10 }
+
+  #validates :name, presence: true  
  
 end
